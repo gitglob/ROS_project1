bf0fae689508934a86de70535327460b5254c601
gitglob <pgrigor22@gmail.com>
2020-10-10 11:15:38 +0200

    Fixed the callback. Everything should work but the arm is going insane

--- scripts/position_finder_class.py
+++ scripts/position_finder_class.py
@@ -17,18 +17,14 @@ class position_finder():
 
 	def __init__(self):
 		# "global" parameters
-		self. cube_index = []
+		self.cube_index = []
 		self.bucket_index = None
 		self.number_of_cubes = 0
 		self.number_of_elements = 0
 		self.cube_pos = geometry_msgs.msg.PoseStamped()
 		self.bucket_pos = geometry_msgs.msg.PoseStamped()
 
-		# topic msgs: [ground_plane, jaco_on_table, cube0, cube1, ... , bucket] [String, Pose, Twist] <= ModelStates
-		self.pose_subscriber = rospy.Subscriber("/gazebo/model_states", ModelStates, self.callback) # initialize pose subscriver
-		self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory) # initialize trajectory publisher
-		self.joint_publisher = rospy.Publisher("/jaco/joint_control", JointState, queue_size=1) # initialize joint state publisher for gripper
-
+		# initialize robot, scene, group, Subscriber, Publishers
 		self.initialize_stuff()
 
 	def initialize_stuff(self): # method to do the environment setup
@@ -51,125 +47,102 @@ class position_finder():
 		print self.robot.get_current_state()
 		print "============"
 
-		# iterate over every cube # THIS PROBABLY NEEDS TO GO IN THE CALLBACK FUNCTION BUT LEAVE IT FOR LATER
-		for i in range(0,self.number_of_cubes):
-			print "============ CUBE <- #", i
-			self.findStuff(i, self.group, self.robot)
+		# topic msgs: [ground_plane, jaco_on_table, cube0, cube1, ... , bucket] [String, Pose, Twist] <= ModelStates
+		self.pose_subscriber = rospy.Subscriber("/gazebo/model_states", ModelStates, self.callback) # initialize pose subscriber
+		self.display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory) # initialize trajectory publisher
+		self.joint_publisher = rospy.Publisher("/jaco/joint_control", JointState, queue_size=1) # initialize joint state publisher for gripper
 
-	def findStuff(self, i, group, robot): # method to loop over every cube
+	def findStuff(self): # method to loop over every cube
 		## Let's setup the planner
 		#group.set_planning_time(1.0)
-		group.set_goal_orientation_tolerance(0.01)
-		group.set_goal_tolerance(0.001)
-		group.set_goal_joint_tolerance(0.01)
-		group.set_num_planning_attempts(100)
+		self.group.set_goal_orientation_tolerance(0.01)
+		self.group.set_goal_tolerance(0.001)
+		self.group.set_goal_joint_tolerance(0.01)
+		self.group.set_num_planning_attempts(100)
 
 		# reset the arm now
-		self.resetArm(group, robot) # first of all, reset the arm
-		if i==0: # the first time we need to open the gripper from the start
+		self.resetArm() # first of all, reset the arm
+		if self.number_of_cubes==0: # the first time we need to open the gripper from the start
 			self.openGripper()
 
-		print "============ Generating plan 1, aka: Grab it by the cuby."
-		pose_goal_1 = group.get_current_pose().pose
-		print 'Cube -> pose goal:', pose_goal_1
+		print "============ Generating plan 1, aka: \nGrab it by the cuby."
+		pose_goal_1 = self.group.get_current_pose().pose
 		
 		waypoints_1 = []
 		waypoints_1.append(pose_goal_1) # current pose
 		pose_goal_1.position = self.cube_pos.pose.position
+		print 'Cube -> pose goal:\n', pose_goal_1.position
 		waypoints_1.append(pose_goal_1) # goal pose
 
 		# create cartesian plan based on current and goal pose
-		(plan_1, fraction_1) = group.compute_cartesian_path(waypoints_1, 0.01, 0.0)
+		(plan_1, fraction_1) = self.group.compute_cartesian_path(waypoints_1, 0.01, 0.0)
 		## Now, we call the planner to compute the plan and visualize it if successful Note that we are just planning, not asking move_group to actually move the robot
-		rospy.sleep(0.5)
+		rospy.sleep(2)
 
 		## You can ask RVIZ to visualize a plan (aka trajectory) for you.  But the group.plan() method does this automatically so this is not that useful here (it just displays the same trajectory again).
 		display_trajectory_1 = moveit_msgs.msg.DisplayTrajectory()
-		display_trajectory_1.trajectory_start = robot.get_current_state()
+		display_trajectory_1.trajectory_start = self.robot.get_current_state()
 		display_trajectory_1.trajectory.append(plan_1)
 		self.display_trajectory_publisher.publish(display_trajectory_1)
 		rospy.sleep(0.5)
 
 		print "==== Executing plan 1."
-		group.execute(plan_1,wait=True)
-		rospy.sleep(0.5)
+		self.group.execute(plan_1,wait=True)
+		rospy.sleep(5)
 
 		# reset the arm again
 		self.closeGripper()
-		self.resetArm(group, robot)
+		self.resetArm()
 
-		print "============ Generating plan 2" # go to the bucket
-		pose_goal_2 = group.get_current_pose().pose
-		print 'Bucket -> pose goal:', pose_goal_2
+		print "============ Generating plan 2: \nReturn to the bucket" # go to the bucket
+		pose_goal_2 = self.group.get_current_pose().pose
 
 		waypoints_2 = []
 		waypoints_2.append(pose_goal_2) # current pose
-		pose_goal_2.position = self.bucket_pos.pose.position
+		pose_goal_2.position.x = self.bucket_pos.pose.position.x
+		pose_goal_2.position.y = self.bucket_pos.pose.position.y
 		pose_goal_2.position.z = 1.35
+		print 'Bucket -> pose goal:\n', pose_goal_2.position
 		waypoints_2.append(copy.deepcopy(pose_goal_2)) # goal pose
 
 		# create cartesian plan based on current and goal pose
-		(plan_2, fraction_2) = group.compute_cartesian_path(waypoints_2, 0.01, 0.0)
-		rospy.sleep(0.5)
+		(plan_2, fraction_2) = self.group.compute_cartesian_path(waypoints_2, 0.01, 0.0)
+		rospy.sleep(2)
 
 		display_trajectory_2 = moveit_msgs.msg.DisplayTrajectory()
-		display_trajectory_2.trajectory_start = robot.get_current_state()
+		display_trajectory_2.trajectory_start = self.robot.get_current_state()
 		display_trajectory_2.trajectory.append(plan_2)
 		self.display_trajectory_publisher.publish(display_trajectory_2)
 		rospy.sleep(0.5)
 
 		print "==== Executing plan 2."
-		group.execute(plan_2,wait=True)
-		rospy.sleep(2)
+		self.group.execute(plan_2,wait=True)
+		rospy.sleep(5)
 
 		# open the gripper to drop the cube
 		self.openGripper()
 
-	def resetArm(self, group, robot):
+	def resetArm(self):
 		print "============ Planning Reset" # go to the original position
-		pose_goal = group.get_current_pose().pose
+		pose_goal = self.group.get_current_pose().pose
 
 		pose_goal.orientation = geometry_msgs.msg.Quaternion(*tf_conversions.transformations.quaternion_from_euler(0., -math.pi/2, 0.))
 		pose_goal.position.x =0.40
 		pose_goal.position.y =-0.10
 		pose_goal.position.z =1.35
-		group.set_pose_target(pose_goal)
+		self.group.set_pose_target(pose_goal)
 
-		plan = group.plan()
-		rospy.sleep(0.5)
+		plan = self.group.plan()
+		rospy.sleep(2)
 
 		display_trajectory = moveit_msgs.msg.DisplayTrajectory()
-		display_trajectory.trajectory_start = robot.get_current_state()
+		display_trajectory.trajectory_start = self.robot.get_current_state()
 		display_trajectory.trajectory.append(plan)
 		self.display_trajectory_publisher.publish(display_trajectory);
 		rospy.sleep(0.5)
 
-		group.go(wait=True)
-		rospy.sleep(0.5)
-
-		if 0: # I abandoned the cartesian command approach, it doesn't work
-			pose_goal = group.get_current_pose().pose
-
-			waypoints = []
-			waypoints.append(pose_goal) # current pose
-			pose_goal.position.x = 0.40
-			pose_goal.position.y = -0.10
-			pose_goal.position.z = 1.55
-			waypoints.append(pose_goal) # goal pose
-
-			# create cartesian plan
-			(plan, fraction) = group.compute_cartesian_path(waypoints, 0.01, 0.0)
-			rospy.sleep(2)
-
-			display_trajectory = moveit_msgs.msg.DisplayTrajectory()
-			display_trajectory.trajectory_start = robot.get_current_state()
-			display_trajectory.trajectory.append(plan)
-			self.display_trajectory_publisher.publish(display_trajectory)
-			rospy.sleep(0.5)
-
-			print "==== Executing Reset."
-			group.execute(plan,wait=True)
-			rospy.sleep(2)
+		self.group.go(wait=True)
+		rospy.sleep(5)
 
 	def openGripper(self):
 		print "============ Opening Grip"
@@ -201,18 +174,25 @@ class position_finder():
 
 	def callback(self, ms): # callback function for 'pose' messages
 		#print "Message: " , ms.pose <- 9 elements[pos, orientation0, ... , orientation8]
-		index = 0
 		if len(ms.name) > self.number_of_elements: # check if new elements have been added and if so, find their position again
-			for str in ms.name:
-				self.number_of_elements+=1 # counter for all the elements in the virtual environment
-				if(str.find('cube')>=0):
-					self.cube_index.append(index)
-					self.cube_pos.pose = ms.pose[index]
-					self.number_of_cubes+=1 # increase cube counter
+			index = 0
+			for str in ms.name: # First get the position of the bucket
 				if(str.find('bucket')>=0):
 					self.bucket_index = index
 					self.bucket_pos.pose = ms.pose[index]
 				index+=1
+			index = 0
+			for str in ms.name: # Now drop all the cubes to the bucket
+				self.number_of_elements+=1 # counter for all the elements in the virtual environment
+				if(str.find('cube')>=0):
+					self.cube_index.append(index) # get the index of every cube
+					self.cube_pos.pose = ms.pose[index] # get the position of every cube in a row
+					# now call the function to actually grab the specific cube
+					print "============ CUBE <- #", self.number_of_cubes
+					self.findStuff()
+					self.number_of_cubes+=1 # increase cube counter
+				index+=1
+			print "============", self.number_of_cubes, "cubes so far...", "============" 
 
 def main(args):
 	'''Initializes and cleanup ros node'''
